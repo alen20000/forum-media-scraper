597fdc27deb45db69852e8397a4a93554025b733
cliff <alen20000>
2026-03-31 13:35:01 +0800

    chore:刪除測試代碼

--- batch.py
+++ batch.py
@@ -1,5 +1,6 @@
 import re
 import config_path
+from pathlib import Path
 
 class Batch:
 
@@ -7,6 +8,16 @@ class Batch:
     def __init__(self):
 
         #初始化
+        
         self.target_list = config_path.BATCH_DOWNLOAD_LIST
+        self.done_list  = config_path.BATCH_DOWNLOAD_LIST_DONE
 
-        self.targer_list.
+        self._check_list()
+    def _check_list(self):
+        '''
+        檢查輸入與完成的list
+        '''
+
+        self.target_list.touch(exist_ok=True)
+        self.done_list.touch(exist_ok=True)
+Batch()
--- config_path.py
+++ config_path.py
@@ -8,7 +8,3 @@ DOWNLOAD_FOLDER = BATH_DIR / "Downloads"
 BATCH_DOWNLOAD_LIST = BATH_DIR / "Bath_Download_List.txt"
 BATCH_DOWNLOAD_LIST_DONE = BATH_DIR / "Your_Done_List.txt"
 
-
-
-print(BATCH_DOWNLOAD_LIST_DONE)
-print(DOWNLOAD_FOLDER)
