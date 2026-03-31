16bc3a1d1af36a8f1370b060927c0c8b20f89242
cliff <alen20000>
2026-03-31 14:13:25 +0800

    feat:完成urls讀取
    之後，要把batch與爬蟲CLI串接

--- batch.py
+++ batch.py
@@ -12,7 +12,15 @@ class Batch:
         self.target_list = config_path.BATCH_DOWNLOAD_LIST
         self.done_list  = config_path.BATCH_DOWNLOAD_LIST_DONE
 
+        #run
+        self._run()
+
+    def _run(self):
+
         self._check_list()
+        self._read_target_list()
+        self._get_urls()
+
     def _check_list(self):
         '''
         檢查輸入與完成的list
@@ -20,4 +28,17 @@ class Batch:
 
         self.target_list.touch(exist_ok=True)
         self.done_list.touch(exist_ok=True)
+
+    def _read_target_list(self):
+
+        self.content =None
+        with open (self.target_list,"r") as f:
+            self.content = f.read()
+
+    def _get_urls(self):
+        pattern = r"https://www.52av[^\s\"\']+"
+
+        urls = re.findall(pattern,self.content)
+        for url in urls:
+            print("目標網址",url)
 Batch()
