640fc293468248afd4b1dbdb166c73312a265d63
cliff <alen20000>
2026-04-01 04:00:43 +0800

    fix : correct file_path typo

--- config_path.py
+++ config_path.py
@@ -5,6 +5,6 @@ BATH_DIR = Path(__file__).resolve().parent
 
 DOWNLOAD_FOLDER = BATH_DIR / "Downloads"
 
-BATCH_DOWNLOAD_LIST = BATH_DIR / "Bath_Download_List.txt"
+BATCH_DOWNLOAD_LIST = BATH_DIR / "Batch_Download_List.txt"
 BATCH_DOWNLOAD_LIST_DONE = BATH_DIR / "Your_Done_List.txt"
 
