0a8d4ed58eb5cb19c8b7cded54788cfc0a4e4bf7
cliff <alen20000>
2026-03-31 12:46:27 +0800

    feat: implement batch list reader using pathlib

--- config_path.py
+++ config_path.py
@@ -1,4 +1,14 @@
 from pathlib import Path
 
-DOWNLOAD_FOLDER = Path('./Download')
 
+BATH_DIR = Path(__file__).resolve().parent
+
+DOWNLOAD_FOLDER = BATH_DIR / "Downloads"
+
+BATCH_DOWNLOAD_LIST = BATH_DIR / "Bath_Download_List.txt"
+BATCH_DOWNLOAD_LIST_DONE = BATH_DIR / "Your_Done_List.txt"
+
+
+
+print(BATCH_DOWNLOAD_LIST_DONE)
+print(DOWNLOAD_FOLDER)
